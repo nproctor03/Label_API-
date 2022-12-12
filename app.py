d60593387af7267ba4218ba16596e6c42cd4e562
nproctor03 <115483700+nproctor03@users.noreply.github.com>
2022-12-12 19:30:30 +0000

    Fix to CORS Error

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, jsonify, request
 import label
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+
 
 
 @app.route('/get_labels', methods=['POST'])
 def get_labels():
     json = request.get_json()
-    # Need to ask if there is a better way to get this.
-    embedding = json['embedding'].get('data').get('embedding')
-    #embedding = embedding['data']
-   # embedding = embedding['embedding']
-    print(embedding)
+    embedding = json['embedding']
+    #print(embedding)
+
 
     status, labels = label.label_image(embedding)
     return jsonify({'msg': status, 'labels': labels})
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=6000)
