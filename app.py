57289a77eb6448f22840023f7b528a7d1e3b39fd
nproctor03 <115483700+nproctor03@users.noreply.github.com>
2022-12-12 19:27:26 +0000

    "change to how embedding is retrieved"

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from flask import Flask, jsonify, request
 import label
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 @app.route('/get_labels', methods=['POST'])
 def get_labels():
     json = request.get_json()
-    embedding = json['embedding']
-    #print(embedding)
-
+    # Need to ask if there is a better way to get this.
+    embedding = json['embedding'].get('data').get('embedding')
+    #embedding = embedding['data']
+   # embedding = embedding['embedding']
+    print(embedding)
 
     status, labels = label.label_image(embedding)
     return jsonify({'msg': status, 'labels': labels})
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=6000)
+    app.run(debug=True, port=5000)
